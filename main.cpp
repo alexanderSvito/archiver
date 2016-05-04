648967ff0be2bf4ba6a7f683a4217196d01eff6e
polluxCast0r <alexandervirk@gmail.com>
2016-05-04 09:21:54 +0300

    Added work with files
    Added test files to check for proper archivation

--- main.cpp
+++ main.cpp
@@ -1,36 +1,38 @@
-#include <iostream>
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <fstream>
+#include <unistd.h>
+#include <fcntl.h>
 
 using namespace std;
+#define SHT_L 32768
 
-int kek=0;
-char input[64];
 int l =64;
-char sheet[1025][64];
+char sheet[SHT_L][64];
 char temp[64];
 int pos,i,m,n;
 
-int search(int quant,int i)
+int search(int quant,int i, char input[128])
 {
     if (quant==2)
     {
         pos = (int)input[i];
     }
     int k=0;
+    if (strlen(input)==0) {
+    	exit(0);
+    }
     for (int j=i; j<i+quant; k++,j++)
     {
         temp[k]=input[j];
     }
     m=quant;
-    for (l=257; l<1025; l++)
+    for (l=257; l<SHT_L; l++)
     {
         if (strcmp(sheet[l],temp)==0)
         {
             pos=l;
-            search(quant+1,i);
+            search(quant+1,i,input);
         }
     }
     for (int f=0; f<65;f++)
@@ -48,26 +50,48 @@ int search(int quant,int i)
 
 int main()
 {
+	char input[128], tem[8];
+	memset(input,0,128);
+	int count=1, sym=0, g=0;
     for (i=0;i<256; i++)
     {
         sheet[i][0]=(char)i;
     };
-    ifstream got("test.md");
-    ofstream output("compressed.md");
-    output<<"256/";
+    int output[300], myFile, outFile;
+    output[0]=256;
     n=258;
-    while (!(got.getline(input,64).eof()))
-    {
-        got>>input;
-        for (i=0; i<64;i=i+m-1)
-        {
-            m=2;
-            output<<search(m,i)<<"/";
-        }
-        memset(input,'\0',64);
+    int o=1;
+    myFile = open("test.md", O_RDONLY);
+    outFile = open("compressed.md", O_WRONLY);
+    while (count!=0) {
+    	//printf("%d\n",count);
+    	count=read(myFile,input,128);
+    	//printf("I've counted\n");
+    	if (strlen(input)==0) {
+    		exit(0);
+    	}
+    	for (i=0; i<128;i=i+m-1,o++)
+	    {
+	    	
+	    	memset(tem, '\0',g);
+	    	g=0;
+	        m=2;
+	        //printf("The input is: %s\n",input);
+	        sym=search(m,i,input);
+	        //printf("%d\n",sym);
+	        while (sym>=256) {
+	        	tem[g]=(char)255;
+	        	sym=sym-256;
+	        	g++;
+	        }
+	        tem[g++]=(char)sym;
+	        tem[g]='\0';
+
+	        write(outFile, tem , 8);
+	    }
     }
-    output<<"257";
-    got.close();
-    output.close();
+    printf("I've finished!\n");
+    close(myFile);
+    close(outFile);
     return 0;
 }
